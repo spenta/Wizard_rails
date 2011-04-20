1e10b59182ea21413550385d43b8968c028d3be5
Thomas Franquelin <thomas.franquelin@spenta.biz>
2011-04-20 19:43:35 +0200

    added next and previous buttons

--- app/helpers/user_requests_helper.rb
+++ app/helpers/user_requests_helper.rb
@@ -2,7 +2,7 @@ module UserRequestsHelper
   def sortable column, title = nil
     title ||= column.titleize
     direction = column == params[:sort] && params[:direction] == "desc" ? "asc" :"desc"
-    link_to title, :sort => column, :direction => direction
+    link_to title, :sort => column, :direction => direction, :start_index => params[:start_index], :num_result => params[:num_result]
   end
 end
 
