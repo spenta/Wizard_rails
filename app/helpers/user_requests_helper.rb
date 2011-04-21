7116a37dd0a8b732bba935d110eb3ee0c55ecc7c
Thomas Franquelin <thomas.franquelin@spenta.biz>
2011-04-21 15:03:45 +0200

    fixed behaviour when less than 5 specs have a gamma > 0  in response

--- app/helpers/user_requests_helper.rb
+++ app/helpers/user_requests_helper.rb
@@ -1,4 +1,6 @@
 module UserRequestsHelper
+  include WizardUtilities
+
   def sortable column, title = nil
     title ||= column.titleize
     direction = column == params[:sort] && params[:direction] == "desc" ? "asc" :"desc"
@@ -9,8 +11,19 @@ module UserRequestsHelper
     gamma_array = gammas.to_a
     #sort by descending value
     gamma_array.sort! {|s2, s1| s1[1] <=> s2[1]}
+    #only keeps positive gammas
+    gamma_array.select!{|s| s[1]>0}
     #array or spec_id by descending importance
     result = gamma_array.collect{|s| s[0]}.first(5)
+    #complete with default important specs. In order
+    # Screen size (speccification_id 5)
+    # CPU (speccification_id 1)
+    # Weight (speccification_id 9)
+    # RAM (speccification_id 2)
+    # HDD (speccification_id 4)
+    default_specs = [5, 1, 9, 2, 4]
+    default_specs.reverse!
+    complete result, :with => default_specs, :until_size_is => 5, :allowing_duplicate => false
+    result
   end
 end
-
--- test/unit/helpers/user_requests_helper_test.rb
+++ test/unit/helpers/user_requests_helper_test.rb
@@ -1,4 +1,37 @@
 require 'test_helper'
 
 class UserRequestsHelperTest < ActionView::TestCase
+  test 'should get important specs from gammas' do
+    gammas_1 = {
+    1 => 10,
+    2 => 10,
+    3 => 20,
+    4 => 10,
+    5 => 0,
+    6 => 0,
+    7 => 0,
+    8 => 13,
+    9 => 15,
+    10 => 18,
+    11 => 30,
+    }
+    result = get_important_specs_id_from(gammas_1)
+    assert  result == [11, 3, 10, 9, 8], "result for gammas_1: #{result}"
+
+    gammas_2 = {
+    1 => 10,
+    2 => 0,
+    3 => 0,
+    4 => 0,
+    5 => 0,
+    6 => 0,
+    7 => 0,
+    8 => 3,
+    9 => 15,
+    10 => 0,
+    11 => 0,
+    }
+    result = get_important_specs_id_from(gammas_2)
+    assert  result == [9, 1, 8, 5, 2], "result for gammas_2: #{result}"
+  end
 end
