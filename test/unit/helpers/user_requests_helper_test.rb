752dc4d4ad87e0d76024777484bedbf1bba206a7
Thomas Franquelin <thomas.franquelin@spenta.biz>
2011-05-05 18:32:03 +0200

    fixed user_request helper test to include the previous modifications

--- test/unit/helpers/user_requests_helper_test.rb
+++ test/unit/helpers/user_requests_helper_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserRequestsHelperTest < ActionView::TestCase
   test 'should get important specs from gammas' do
     gammas_1 = {
-    1 => 10,
+    11 => 10,
     2 => 10,
     3 => 20,
     4 => 10,
@@ -13,13 +13,13 @@ class UserRequestsHelperTest < ActionView::TestCase
     8 => 13,
     9 => 15,
     10 => 18,
-    11 => 30,
+    12 => 30,
     }
     result = get_important_specs_id_from(gammas_1)
-    assert  result == [11, 3, 10, 9, 8], "result for gammas_1: #{result}"
+    assert  result == [12, 3, 10, 9, 8], "result for gammas_1: #{result}"
 
     gammas_2 = {
-    1 => 10,
+    11 => 10,
     2 => 0,
     3 => 0,
     4 => 0,
@@ -29,11 +29,11 @@ class UserRequestsHelperTest < ActionView::TestCase
     8 => 3,
     9 => 15,
     10 => 0,
-    11 => 0,
+    12 => 0,
     }
 
     result = get_important_specs_id_from(gammas_2)
-    assert  result == [9, 1, 8, 5, 2], "result for gammas_2: #{result}"
+    assert  result == [9, 11, 8, 5, 2], "result for gammas_2: #{result}"
   end
 
   test 'should give right color numbers' do
