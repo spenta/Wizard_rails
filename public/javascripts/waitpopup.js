d36fa2a13141e4013bc3358cc34a7808d6f52b5a
Thomas <thomas.franquelin@spenta.biz>
2011-05-10 16:27:12 +0200

    Added fade in effects on the loading popup

--- public/javascripts/waitpopup.js
+++ public/javascripts/waitpopup.js
@@ -4,12 +4,18 @@ $j(document).ready(
     function() {
         $j('#popup').hide();
         $j('#overlay').hide();
+        $j('#wait_1').hide();
+        $j('#wait_2').hide();
+        $j('#wait_3').hide();
 
         $j('.next-button').click(
             function()
             {
                 $j('#popup').toggle();
                 $j('#overlay').toggle();
+                $j('#wait_1').delay(600).fadeIn('slow');
+                $j('#wait_2').delay(1800).fadeIn('slow');
+                $j('#wait_3').delay(2400).fadeIn('slow');
             }
             );
     }
