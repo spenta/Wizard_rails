04f5ee9d069039c3830094bff5289c29632a6d5f
Vincent Porcel <vincentporcel@Vincent-Porcel-Macbook.local>
2011-06-05 12:04:16 +0200

    Result page improvement 2

--- public/javascripts/result.js
+++ public/javascripts/result.js
@@ -1,20 +1,20 @@
 $j = jQuery.noConflict();
 // ----- Toggle pour dérouler les caractéristiques des PC stars -----
-$j(document).ready(
-    function() {
-      $j("#Collapsable").hide();
-      $j("#HideButton").hide();
+//$j(document).ready(
+//    function() {
+//      $j("#Collapsable").hide();
+//      $j("#HideButton").hide();
 
-      $j('#ExpandButton,#HideButton').click(
-        function()
-        {
-          $j('#Collapsable').toggle();
-          $j('#ExpandButton').toggle();
-          $j('#HideButton').toggle();
-        }
-        );
-    }
-    );
+//      $j('#ExpandButton,#HideButton').click(
+//        function()
+//        {
+//          $j('#Collapsable').toggle();
+//          $j('#ExpandButton').toggle();
+//          $j('#HideButton').toggle();
+//        }
+//        );
+//    }
+//    );
 
 // ----- Tri de la table -----
   $j(document).ready(
