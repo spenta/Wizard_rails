ed70a9e1486bdfb6e63f005c2682f041c7f07df1
Thomas <thomas.franquelin@spenta.biz>
2011-06-07 18:01:21 +0200

    Improved the home page (for SEO on Comment choisir un PC

---
+++ public/javascripts/home.js
@@ -0,0 +1,18 @@
+$j(function() {
+  $j('#retailer_logos').ulslide({
+    duration:1000,
+    autoslide:800,
+    width:268,
+    height:100
+  });
+});
+
+$j(function() {
+  $j('#brand_logos').ulslide({
+    duration:1000,
+    autoslide:800,
+    width:268,
+    height:100
+  });
+});
+
