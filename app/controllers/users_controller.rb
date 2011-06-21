2c6820e4e1f8b79c52d351900b641c862b63a0e6
Thomas <thomas.franquelin@spenta.biz>
2011-06-21 19:58:33 +0200

    Added a simple auth system : users can be added only if there is no user or if a user is already logged in (if so, "admin" appears at the bottom right of the footer").

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
 class UsersController < ApplicationController
+
   def new
-    @user = User.new
+    unless User.count == 0 or current_user
+      redirect_to :root
+    else
+      @user = User.new
+    end
   end
 
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to root_url, :notice => "Signed up!"
+      redirect_to root_url
     else
       render "new"
     end
