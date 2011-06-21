b70d804b632ac5daa95e959e62913efaefd0fcc7
Thomas <thomas.franquelin@spenta.biz>
2011-06-21 16:58:00 +0200

    Added https for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,8 @@ WizardRails::Application.routes.draw do
   get "logout" => "sessions#destroy", :as => "logout"
   get "login" => "sessions#new", :as => "login"
   get "signup" => "users#new", :as => "signup"
-  resources :users
-  resources :sessions
+  resources :users, :constraints => { :protocol => "https" }
+  resources :sessions, :constraints => { :protocol => "https" }
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
